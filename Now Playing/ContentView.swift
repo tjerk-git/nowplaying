a847d401a4b36e8d572d30569b277c6896aef9a5
Tjerk Dijkstra <tjerk@hey.com>
2021-05-17 09:08:57 +0200

    Initial Commit

---
+++ Now Playing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Now Playing
+//
+//  Created by Tjerk Dijkstra on 17/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Now Playing/Now_PlayingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Now_PlayingApp.swift
+//  Now Playing
+//
+//  Created by Tjerk Dijkstra on 17/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Now_PlayingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
